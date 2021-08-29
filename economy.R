94fdd0458c5408e5193ebb0196a67f4cc64f63f2
Ben Zigterman <ben@bzigterman.com>
2021-08-28 21:31:36 -0500

    rename some variables

--- economy.R
+++ economy.R
@@ -5,23 +5,23 @@ library(lubridate)
 twenty_years_ago <- ymd((today() - years(20)))
 
 # unemployment rate ----
-unemployment_rate <- Quandl("FRED/UNRATE") 
-recent_unemployment_rate <- unemployment_rate %>%
+data <- Quandl("FRED/UNRATE") 
+recent_data <- data %>%
   filter(Date > twenty_years_ago) %>%
   mutate(short_date = paste(month(Date, label = TRUE, abbr = FALSE),
                             mday(Date))) 
 
-ggplot(recent_unemployment_rate, aes(x = as.Date(Date),
+ggplot(recent_data, aes(x = as.Date(Date),
                                       y = Value)) +
   geom_line() +
   labs(title = "Unemployment Rate",
        caption = paste("Source: Quandl. Data updated",
-                       head(recent_unemployment_rate$short_date,1))) +
+                       head(recent_data$short_date,1))) +
   xlab(NULL) +
   ylab(NULL) +
   scale_x_date(expand = expansion(mult = c(0, .01))) +
   scale_y_continuous(position = "right",
-                     limits = c(0,max(recent_unemployment_rate$Value)*1.05)) +
+                     limits = c(0,max(recent_data$Value)*1.05)) +
   theme(axis.text.y = element_text(size = 10),
         axis.text.x = element_text(size = 8),
         panel.grid.minor = element_blank(),
@@ -33,6 +33,35 @@ ggplot(recent_unemployment_rate, aes(x = as.Date(Date),
 
 ggsave("plots/unemployment_rate.png", width = 8, height = 8*(628/1200), dpi = 320)
 
+# real GPD ----
+# 
+# data <- Quandl("FRED/GDPC1") 
+# recent_data <- data %>%
+#   filter(Date > twenty_years_ago) %>%
+#   mutate(short_date = paste(month(Date, label = TRUE, abbr = FALSE),
+#                             mday(Date))) 
+# 
+# ggplot(recent_data, aes(x = as.Date(Date),
+#                                      y = Value)) +
+#   geom_line() +
+#   labs(title = "Real GDP",
+#        caption = paste("Source: Quandl. Data updated",
+#                        head(recent_data$short_date,1))) +
+#   xlab(NULL) +
+#   ylab(NULL) +
+#   scale_x_date(expand = expansion(mult = c(0, .01))) +
+#   scale_y_continuous(position = "right",
+#                      limits = c(0,max(recent_data$Value)*1.05)) +
+#   theme(axis.text.y = element_text(size = 10),
+#         axis.text.x = element_text(size = 8),
+#         panel.grid.minor = element_blank(),
+#         panel.background = element_blank(),
+#         panel.grid.major.y = element_line(colour = "grey93"),
+#         strip.text = element_text(size = 11),
+#         strip.background = element_blank(),
+#         plot.caption = element_text(colour = "grey40"))
+# 
+# ggsave("plots/unemployment_rate.png", width = 8, height = 8*(628/1200), dpi = 320)
 
 # consumer sentiment ----
 consumer_sentiment <- Quandl("UMICH/SOC1") 
