d67d27991754f691b879504db27ba1837d53a921
Ben Zigterman <ben@bzigterman.com>
2021-08-29 11:41:37 -0500

    add employment charts

--- economy.R
+++ economy.R
@@ -17,7 +17,7 @@ recent_data <- data %>%
 ggplot(recent_data, aes(x = date,
                         y = value/100)) +
   geom_line() +
-  labs(title = "U.S. Unemployment Rate",
+  labs(title = "Unemployment Rate",
        caption = paste("Source: FRED. Data updated",
                        tail(recent_data$short_date,1))) +
   xlab(NULL) +
@@ -36,6 +36,60 @@ ggplot(recent_data, aes(x = date,
         plot.caption = element_text(colour = "grey40"))
 
 ggsave("plots/unemployment_rate.png", width = 8, height = 8*(628/1200), dpi = 320)
+
+# employment -----
+data <- fredr(series_id = "PAYEMS")
+recent_data <- data %>%
+  filter(date > twenty_years_ago) %>%
+  mutate(short_date = paste(month(date, label = TRUE, abbr = FALSE),
+                            mday(date))) %>%
+  mutate(change = value - lag(value))
+
+ggplot(recent_data, aes(x = date,
+                        y = value/1000)) +
+  geom_line() +
+  labs(title = "Total Nonfarm Payroll",
+       caption = paste("Source: FRED. Data updated",
+                       tail(recent_data$short_date,1))) +
+  xlab(NULL) +
+  ylab(NULL) +
+  scale_x_date(expand = expansion(mult = c(0, .01))) +
+  scale_y_continuous(position = "right",
+                     labels = label_comma(suffix = "M"),
+                     limits = c(0,max(recent_data$value/1000)*1.05)) +
+  theme(axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 8),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey93"),
+        strip.text = element_text(size = 11),
+        strip.background = element_blank(),
+        plot.caption = element_text(colour = "grey40"))
+
+ggsave("plots/employment.png", width = 8, height = 8*(628/1200), dpi = 320)
+
+## employment change ----
+ggplot(recent_data, aes(x = date,
+                        y = change*1000)) +
+  geom_col() +
+  labs(title = "Change in Total Nonfarm Payroll",
+       caption = paste("Source: FRED. Data updated",
+                       tail(recent_data$short_date,1))) +
+  xlab(NULL) +
+  ylab(NULL) +
+  scale_x_date(expand = expansion(mult = c(0, .01))) +
+  scale_y_continuous(position = "right",
+                     labels = label_comma()) +
+  theme(axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 8),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey93"),
+        strip.text = element_text(size = 11),
+        strip.background = element_blank(),
+        plot.caption = element_text(colour = "grey40"))
+
+ggsave("plots/employment_change.png", width = 8, height = 8*(628/1200), dpi = 320)
 
 # real GPD ----
 
@@ -134,7 +188,13 @@ title: Economic Indicators
 permalink: /charts/economy/
 ---
 
+## United States
+
 ![Unemployment Rate](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/unemployment_rate.png)
+
+![Total Nonfarm Payroll](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/employment.png)
+
+![Total Nonfarm Payroll Change](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/employment_change.png)
 
 ![Real GDP](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/real_gdp.png)
 
