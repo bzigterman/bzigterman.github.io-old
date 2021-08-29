fbe9310110398bcab6e0e5b785f12083b921fc33
Ben Zigterman <ben@bzigterman.com>
2021-08-29 12:57:10 -0500

    add median income

--- charts/economy.md
+++ charts/economy.md
@@ -12,6 +12,8 @@ permalink: /charts/economy/
 
 ![Total Nonfarm Payroll Change](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/employment_change.png)
 
+![Real Median Income](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/real_median_income.png)
+
 ![Real GDP](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/real_gdp.png)
 
 ![Real GDP Growth](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/real_gdp_growth.png)
--- economy.R
+++ economy.R
@@ -91,17 +91,45 @@ ggplot(recent_data, aes(x = date,
 
 ggsave("plots/employment_change.png", width = 8, height = 8*(628/1200), dpi = 320)
 
-# real GPD ----
+# median household income ----
+data <-fredr(series_id = "MEHOINUSA672N")
+recent_data <- data %>%
+  filter(date > twenty_years_ago) %>%
+  mutate(short_date = paste(month(date, label = TRUE, abbr = FALSE),
+                            mday(date))) 
 
+ggplot(recent_data, aes(x = as.Date(date),
+                        y = value)) +
+  geom_line() +
+  labs(title = "Real Median Household Income",
+       caption = paste("Source: FRED. Data updated",
+                       tail(recent_data$short_date,1))) +
+  xlab(NULL) +
+  ylab(NULL) +
+  scale_x_date(expand = expansion(mult = c(0, .01))) +
+  scale_y_continuous(position = "right",
+                     labels = label_dollar(),
+                     limits = c(0,max(recent_data$value)*1.05)) +
+  theme(axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 8),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey93"),
+        strip.text = element_text(size = 11),
+        strip.background = element_blank(),
+        plot.caption = element_text(colour = "grey40"))
+
+ggsave("plots/real_median_income.png", width = 8, height = 8*(628/1200), dpi = 320)
+
+# real GDP ----
 data <-fredr(series_id = "GDPC1")
 recent_data <- data %>%
   filter(date > twenty_years_ago) %>%
   mutate(short_date = paste(month(date, label = TRUE, abbr = FALSE),
-                            mday(date))) %>%
-  mutate(scaled_value = value/1000) 
+                            mday(date))) 
 
 ggplot(recent_data, aes(x = as.Date(date),
-                        y = scaled_value)) +
+                        y = value/1000)) +
   geom_line() +
   labs(title = "Real GDP",
        caption = paste("Source: FRED. Data updated",
@@ -111,7 +139,7 @@ ggplot(recent_data, aes(x = as.Date(date),
   scale_x_date(expand = expansion(mult = c(0, .01))) +
   scale_y_continuous(position = "right",
                      labels = label_dollar(suffix = "T"),
-                     limits = c(0,max(recent_data$scaled_value)*1.05)) +
+                     limits = c(0,max(recent_data$value/1000)*1.05)) +
   theme(axis.text.y = element_text(size = 10),
         axis.text.x = element_text(size = 8),
         panel.grid.minor = element_blank(),
@@ -195,6 +223,8 @@ permalink: /charts/economy/
 ![Total Nonfarm Payroll](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/employment.png)
 
 ![Total Nonfarm Payroll Change](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/employment_change.png)
+
+![Real Median Income](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/real_median_income.png)
 
 ![Real GDP](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/real_gdp.png)
 
