819a80e11492df4004523c53f245d339b83e4203
Ben Zigterman <ben@bzigterman.com>
2021-08-28 21:14:20 -0500

    adjust text

--- economy.R
+++ economy.R
@@ -70,8 +70,6 @@ title: Economic Indicators
 permalink: /charts/economy/
 ---
 
-Here are some economic indicators I like to keep track of:
-
 ![Unemployment Rate](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/unemployment_rate.png)
 
 ![Consumer Sentiment](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/consumer_sentiment.png)
