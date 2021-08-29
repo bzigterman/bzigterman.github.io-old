76c620002b2522ffb2d7fa29ac9420236265abdd
Ben Zigterman <ben@bzigterman.com>
2021-08-28 21:06:51 -0500

    add unem_rate to econ

--- charts/economy.md
+++ charts/economy.md
@@ -6,7 +6,7 @@ permalink: /charts/economy/
 
 Here are some economic indicators I like to keep track of:
 
-University of Michigan Consumer Survey, Index of Consumer Sentiment
+![Unemployment Rate](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/unemployment_rate.png)
 
 ![Consumer Sentiment](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/consumer_sentiment.png)
 
--- economy.R
+++ economy.R
@@ -4,6 +4,37 @@ library(lubridate)
 
 twenty_years_ago <- ymd((today() - years(20)))
 
+# unemployment rate ----
+unemployment_rate <- Quandl("FRED/UNRATE") 
+recent_unemployment_rate <- unemployment_rate %>%
+  filter(Date > twenty_years_ago) %>%
+  mutate(short_date = paste(month(Date, label = TRUE, abbr = FALSE),
+                            mday(Date))) 
+
+ggplot(recent_unemployment_rate, aes(x = as.Date(Date),
+                                      y = Value)) +
+  geom_line() +
+  labs(title = "Unemployment Rate",
+       caption = paste("Source: Quandl. Data updated",
+                       head(recent_unemployment_rate$short_date,1))) +
+  xlab(NULL) +
+  ylab(NULL) +
+  scale_x_date(expand = expansion(mult = c(0, .01))) +
+  scale_y_continuous(position = "right",
+                     limits = c(0,max(recent_unemployment_rate$Value)*1.05)) +
+  theme(axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 8),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey93"),
+        strip.text = element_text(size = 11),
+        strip.background = element_blank(),
+        plot.caption = element_text(colour = "grey40"))
+
+ggsave("plots/unemployment_rate.png", width = 8, height = 8*(628/1200), dpi = 320)
+
+
+# consumer sentiment ----
 consumer_sentiment <- Quandl("UMICH/SOC1") 
 recent_consumer_sentiment <- consumer_sentiment %>%
   filter(Date > twenty_years_ago) %>%
@@ -41,7 +72,7 @@ permalink: /charts/economy/
 
 Here are some economic indicators I like to keep track of:
 
-University of Michigan Consumer Survey, Index of Consumer Sentiment
+![Unemployment Rate](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/unemployment_rate.png)
 
 ![Consumer Sentiment](https://raw.githubusercontent.com/bzigterman/bzigterman.github.io/master/plots/consumer_sentiment.png)
 ",
