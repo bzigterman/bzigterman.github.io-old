ff28c7c1ebc95ab53cb9f83f8c46a9e9200e0ca3
Ben Zigterman <ben@bzigterman.com>
2021-09-06 20:07:11 -0500

    rearrange charts

--- economy.R
+++ economy.R
@@ -540,6 +540,12 @@ title: Economic Indicators
 permalink: /charts/economy/
 ---
 
+## Champaign
+
+![Unemployment Rate]({{ site.baseurl }}/plots/champaign_unemployment_rate.png)
+
+![Employment]({{ site.baseurl }}/plots/champaign_employment.png)
+
 ## United States
 
 ![Unemployment Rate]({{ site.baseurl }}/plots/unemployment_rate.png)
@@ -555,12 +561,6 @@ permalink: /charts/economy/
 ![Durable Goods]({{ site.baseurl }}/plots/durable_goods.png)
 
 ![Consumer Sentiment]({{ site.baseurl }}/plots/consumer_sentiment.png)
-
-## Champaign
-
-![Unemployment Rate]({{ site.baseurl }}/plots/champaign_unemployment_rate.png)
-
-![Employment]({{ site.baseurl }}/plots/champaign_employment.png)
 
 Data retrieved from the [Federal Reserve Bank of St. Louis](https://fred.stlouisfed.org)
 ",
