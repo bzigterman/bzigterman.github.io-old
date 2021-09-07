d7b2838798891a2014535c80630f5e8ee3a539c8
Ben Zigterman <ben@bzigterman.com>
2021-09-06 19:50:09 -0500

    try to get an mlb chart started

--- mlb.R
+++ mlb.R
@@ -3,6 +3,8 @@ library(lubridate)
 library(scales)
 library(httr)
 library(rio)
+library(mlbstatsR)
+library(reticulate)
 
 # get data
 mlb_host_url <- "http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1&teamId=145"
@@ -18,3 +20,13 @@ game$teams[[145]]
 
 
 game[["dates"]][[1]][["games"]][[1]][["officialDate"]]
+
+mlb_team_stats(2021, "pitching", "regular")
+
+
+standings <- get_reference_team_standings(2021)
+
+
+py_install("MLB-StatsAPI", pip = TRUE)
+py_run_string("import statsapi")
+py_run_file("mlb.py")
