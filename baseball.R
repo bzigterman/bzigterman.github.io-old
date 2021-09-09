52cf3b5aa889336a0ca30f724015431a42b4ef89
Ben Zigterman <ben@bzigterman.com>
2021-09-09 12:25:50 -0500

    Update baseball.R

--- baseball.R
+++ baseball.R
@@ -142,7 +142,7 @@ permalink: /charts/baseball/
 
 ![CHW]({{ site.baseurl }}/plots/mlb_wins_losses.png)
 
-Data from [FiveThirtyEight](https://github.com/fivethirtyeight/data/tree/master/mlb-elo). Inspired by [gkaramanis](https://github.com/gkaramanis/remakes/tree/master/wwc-remake).
+Data from [FiveThirtyEight](https://github.com/fivethirtyeight/data/tree/master/mlb-elo).
 
 ",
 sep = ""
